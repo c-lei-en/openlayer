e374569e6264ceac1548b8e4a8354e833c73fafa
纯洁 <1762928628@qq.com>
2019-11-04 20:21:22 +0800

    api

--- src/api/request.js
+++ src/api/request.js
@@ -1,14 +1,30 @@
 import axios from 'axios';
 import Vue from 'vue';
 
-axios.defaults.baseURL = '/api';
-axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.baseURL = '/url';
 Vue.config.productionTip = false;
+Vue.config.devlopmentTip = false;
+
+export const GetUser = (name, password) => {
+    return axios.get(`/api/services/app/Register/GetUserByName?Name=${name}&Password=${password}`)
+}
 
 export const GetMountain = () => {
-    return axios.get('/mountain.json')
+    return axios.get(`/mountain.json`)
 }
 
 export const GetDaoguan = () => {
-    return axios.get('/daoguan.json')
+    return axios.get(`/daoguan.json`)
+}
+
+export const GetMountainIfm = (name) => {
+    return axios.get(`/app/Mountain/GetMountainByName?Name=${name}`)
+}
+
+export const GetDaoguanIfm = (name) => {
+    return axios.get(`/api/services/app/Palace/GetPalaceByName?Name${name}`)
+}
+
+export const GetDynasty = (name) => {
+    return axios.get(`/api/services/app/Dynasty/GetDynastyByDnt?Dnt=${name}`)
 }
--- src/main.js
+++ src/main.js
@@ -5,11 +5,11 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-import axios from 'axios'
 // import echarts from 'echarts'
 
 Vue.config.productionTip = false;
-Vue.prototype.$axios = axios;
+
+Vue.config.devlopmentTip = false;
 
 
 // Vue.prototype.$echarts = echarts;
