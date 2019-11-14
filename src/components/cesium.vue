1c99497ed5d9b12e383ff3c32d5c618ced839e6c
c-lei-cn <1762928628@qq.com>
2019-11-14 16:46:18 +0800

    获取几何体

--- src/components/cesium.vue
+++ src/components/cesium.vue
@@ -29,13 +29,28 @@ export default {
 
     let box = this.viewer.entities.add({
         name: 'box',
+        id: 'box',
         position: this.Cesium.Cartesian3.fromDegrees(this.$route.params.coordinate[0], this.$route.params.coordinate[1]),
         box: {
-            dimensions: new this.Cesium.Cartesian3(10.0, 10.0, 10.0),
+            dimensions: new this.Cesium.Cartesian3(1000.0, 1000.0, 1000.0),
 			material: this.Cesium.Color.RED.withAlpha(0.5),
         }
     });
     this.viewer.zoomTo(this.viewer.entities);
+    //创建一个点击事件
+        var handler = new this.Cesium.ScreenSpaceEventHandler(this.viewer.scene.canvas);
+
+        //设置单击事件的处理函数
+        handler.setInputAction( (e) => {
+
+            //用来获取被点击的几何
+            var pick = this.viewer.scene.pick(e.position);
+
+            //判断   确定点击的地方有创建的几何并且点击的几何id为rectangle-1
+            if (this.Cesium.defined(pick)&&(pick.id._id === 'box')){
+                console.log(this.$route.params.queryIfm)
+            }
+        }, this.Cesium.ScreenSpaceEventType.LEFT_CLICK)
   },
   methods: {}
 };
